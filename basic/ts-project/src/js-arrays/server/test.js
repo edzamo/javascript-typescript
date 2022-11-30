5799497ea74c3a4c27b12a1bd75e68e3c7408042
edzamo13@gmail.com <ezamora@mag.gob.ec>
2022-11-30 08:14:07 -0500

    improve the course structure

---
+++ basic/ts-project/src/js-arrays/server/test.js
@@ -0,0 +1,11 @@
+const array = [['🐸','🐱'],'🐹', ['🐯']];
+const rta = array.flat();
+
+console.log(rta.findIndex(i=> i==='🐸'));
+
+
+const array1 = ['a', 'bb', 'ccc'];
+const rta1 = array1.map(item => item.length);
+
+console.log(rta1);
+
