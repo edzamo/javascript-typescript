c9133c6f659614d0890b99c8752c9b4bc9c8c127
edzamo13@gmail.com <ezamora@mag.gob.ec>
2022-12-13 19:43:20 -0500

    add clean code

---
+++ cleanCode/designpattern/dist/example.js
@@ -0,0 +1,4 @@
+"use strict";
+const a = 'hi!.. world';
+const b = 'nest word ';
+console.log(`This is a mi first var: ${a} - ${b}`);
---
+++ cleanCode/designpattern/src/example.ts
@@ -0,0 +1,4 @@
+const a: string = 'hi!.. world'
+const b: string = 'nest word '
+
+console.log(`This is a mi first var: ${a} - ${b}`)
